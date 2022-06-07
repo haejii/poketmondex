ad74f1a47b070966e64af0875eef81a84b9c6ab1
양혜지 <haejidev@gmail.com>
2022-06-08 08:55:12 +0900

    Implement Initial styling and connect Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import IndexPage from './pages/IndexPage';
+import DetailPage from './pages/DetailPage';
 
 function App() {
   return (
-    <div>
-      Hello React-Query !
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={IndexPage} />
+        <Route path="/:id" component={DetailPage} />
+      </Switch>
+    </Router>
   );
 }
 
